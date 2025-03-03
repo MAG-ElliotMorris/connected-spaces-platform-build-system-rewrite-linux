e740eb31b2447fec0c1a3b42a43b3df6f3ca4bfe
Elliot <elliot.morris@magnopus.com>
2025-03-03 20:09:38 +0000

    Upload artifact after build

--- Library/premake_install_msvc.lua
+++ Library/premake_install_msvc.lua
@@ -5,25 +5,13 @@ function ConfigInstallForMSVC()
     
     local topLevelDir = "%{wks.location}/../" -- wks.location will be /build here.
 
-    local copyLibCommand = 'xcopy /Y "%{cfg.buildtarget.abspath}" "' .. topLevelDir .. 'install\\lib\\CSP.lib'
-    local copyDLLCommand = 'xcopy /Y "%{cfg.buildtarget.abspath}" "' .. topLevelDir .. 'install\\lib\\CSP.dll'
+    local copyLibCommand = 'xcopy /E /I /Y /H "' .. topLevelDir .. 'bin" "' .. topLevelDir .. 'install\\bin'
+    local copyHeadersCommand = 'xcopy /E /I /Y /H "' .. topLevelDir .. 'include\\CSP" "' .. topLevelDir .. 'install\\include"'
 
-    local copyHeadersCommand = 'xcopy /E /I /Y "' .. topLevelDir .. 'include\\CSP" "' .. topLevelDir .. 'install\\include"'
-
-    if _OPTIONS["build-type"] == "shared" then
-        postbuildcommands {
-            'mkdir "' .. topLevelDir .. 'install\\lib"',
-            'mkdir "' .. topLevelDir .. 'install\\include"',
-            copyDLLCommand,
-            copyLibCommand,
-            copyHeadersCommand
-        }
-    else
-        postbuildcommands {
-            'mkdir "' .. topLevelDir .. 'install\\lib"',
-            'mkdir "' .. topLevelDir .. 'install\\include"',
-            copyLibCommand,
-            copyHeadersCommand
-        }
-    end
+    postbuildcommands {
+        'mkdir "' .. topLevelDir .. 'install\\bin"',
+        'mkdir "' .. topLevelDir .. 'install\\include"',
+        copyLibCommand,
+        copyHeadersCommand
+    }
 end
