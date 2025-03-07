8b53ec954a899302389c4d52166a98da233b2164
Elliot Morris <Elliot.Morris@magnopus.com>
2025-03-07 12:44:16 +0000

    Missed a quote, that was the whole issue
    :scream:

--- Library/premake_install_msvc.lua
+++ Library/premake_install_msvc.lua
@@ -5,12 +5,15 @@ function ConfigInstallForMSVC()
     
     local topLevelDir = "%{wks.location}/../" -- wks.location will be /build here.
 
-    local copyLibCommand = 'xcopy /E /I /Y /H "' .. topLevelDir .. 'bin" "' .. topLevelDir .. 'install\\bin'
-    local copyHeadersCommand = 'xcopy /E /I /Y /H "' .. topLevelDir .. 'include\\CSP" "' .. topLevelDir .. 'install\\include"'
+    local copyLibCommand = 'xcopy /E /I /Y /H "' .. topLevelDir .. 'bin" "' .. topLevelDir .. 'install/bin"'
+    local copyHeadersCommand = 'xcopy /E /I /Y /H "' .. topLevelDir .. 'include/CSP" "' .. topLevelDir .. 'install/include"'
+
+    print(topLevelDir)
+    print(copyLibCommand)
 
     postbuildcommands {
-        'mkdir "' .. topLevelDir .. 'install\\bin"',
-        'mkdir "' .. topLevelDir .. 'install\\include"',
+        'mkdir "' .. topLevelDir .. 'install/bin"',
+        'mkdir "' .. topLevelDir .. 'install/include"',
         copyLibCommand,
         copyHeadersCommand
     }
